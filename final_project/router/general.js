353e4d92f22a6fba2d1cf4e42d3c377534ccbc18
kamalscraping-collab <kamalscraping@gmail.com>
2025-12-29 18:24:37 -0500

    Have done task 4

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -33,8 +33,13 @@ public_users.get('/author/:author',function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    for (num in books){
+        let book = books[num];
+        title = book['title'];
+        if (title === req.params.title){
+            res.send(book);
+        }
+    }
 });
 
 //  Get book review
