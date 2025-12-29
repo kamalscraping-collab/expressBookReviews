3dcb1575787380eb52c4023f0959f32d7f4ba341
kamalscraping-collab <kamalscraping@gmail.com>
2025-12-29 18:02:58 -0500

    Have done tasks 1, 2 and 3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,20 +12,23 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    return res.json(books);
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+  return res.send(books[req.params.isbn]);
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    for (num in books){
+        let book = books[num];
+        author = book['author'];
+        if (author === req.params.author){
+            res.send(book);
+        }
+    }
 });
 
 // Get all books based on title
