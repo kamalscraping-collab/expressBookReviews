7ca5f48504c6ff7f81e0e6a80bed07274a108071
kamalscraping-collab <Kamalscraping@gmail.com>
2026-01-06 13:58:15 -0500

    Completed Task 9

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -13,9 +13,15 @@ const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
 }
 
+// MINE
 //only registered users can login
 regd_users.post("/login", (req,res) => {
     const user = req.body.user;
+    
+    const username = user.username;
+    // console.log(`Query: ${req.query}`);
+    console.log(`User: ${user.username}`);
+    // console.log(`Username: ${username}`)
     if (!user) {
         return res.status(404).json({ message: "Body Empty" });
     }
@@ -25,7 +31,7 @@ regd_users.post("/login", (req,res) => {
     }, 'access', { expiresIn: 60 * 60 });
     // Store access token in session
     req.session.authorization = {
-        accessToken
+        accessToken, username
     }
     return res.status(200).send("User successfully logged in");
     
@@ -58,6 +64,33 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
 
 });
 
+// Endpoint to delete review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const isbn = req.params.isbn;
+    const username = req.session.authorization.username; // Retrieve username from session
+    const book = books[isbn];
+    const bookReviews = book.reviews;
+
+    console.log(`Logged in user: ${username}`);
+
+    if (!book) {
+        res.send(`Book with isbn ${isbn} not found.`);
+    } else if (!username) {
+        res.send(`User is not authenticated. Please log in.`);
+    } else {
+        const userReviewIndex = bookReviews.findIndex(review => review.username === username);
+
+        if (userReviewIndex !== -1) {
+            // If user has a review, delete the review
+            bookReviews.splice(userReviewIndex, 1);
+            res.send(`Review deleted for user ${username} on book with isbn ${isbn}`);
+        } else {
+            // If user doesn't have a review on the book
+            res.send(`The user ${username} has no review on book with isbn ${isbn}`);
+        }
+    }
+
+});
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
 module.exports.users = users;
